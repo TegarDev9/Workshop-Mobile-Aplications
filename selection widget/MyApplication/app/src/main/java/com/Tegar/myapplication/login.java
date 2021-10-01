870d3f2afa94f7abcdc88ca1ec70ab92afb30343
Tegar karunia ilham_E41200988 <86245238+TegarDev9@users.noreply.github.com>
2021-10-01 09:14:06 +0700

    selection widget

---
+++ selection widget/MyApplication/app/src/main/java/com/Tegar/myapplication/login.java
@@ -0,0 +1,27 @@
+package com.Tegar.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        buttonLogin = findViewById(R.id.btnLogin);
+       buttonLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent pindah = new Intent(login.this, Menu.class);
+               startActivity(pindah);
+           }
+       });
+    }
+    }
