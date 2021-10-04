586971488cf7d77b89df6e9d0e299bc38ec4568e
Tegar karunia ilham_E41200988 <86245238+TegarDev9@users.noreply.github.com>
2021-10-04 23:12:57 +0700

    penjelasan singkat code di setiap file

--- selection widget/my spiner/app/src/main/java/com/tegar/myspiner/MainActivity.java
+++ selection widget/my spiner/app/src/main/java/com/tegar/myspiner/MainActivity.java
@@ -10,20 +10,23 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /**membuat method spinner */
     Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /**Untuk menampilkan id dari layout*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
-
+     /**Membuat Adapter*/
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
+            /**membuat Toas*/
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(parent.getContext(),"Anda Klick negara:"+parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
 
