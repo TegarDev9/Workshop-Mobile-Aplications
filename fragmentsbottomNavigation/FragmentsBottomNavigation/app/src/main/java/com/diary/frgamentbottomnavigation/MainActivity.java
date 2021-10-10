bc70c9c07dfcb0ce15ad0ac531048f3f06e1058e
Tegar karunia ilham_E41200988 <86245238+TegarDev9@users.noreply.github.com>
2021-10-10 09:02:35 +0700

    Menjelaskan singkat souce code dan images

--- fragmentsbottomNavigation/FragmentsBottomNavigation/app/src/main/java/com/diary/frgamentbottomnavigation/MainActivity.java
+++ fragmentsbottomNavigation/FragmentsBottomNavigation/app/src/main/java/com/diary/frgamentbottomnavigation/MainActivity.java
@@ -11,20 +11,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-//Memberikan action ke battom navigation
+//Memberikan action ke bottom navigation
     BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
         bottomNavigation = findViewById ( R.id.bottom_navigation );
-        //berfungsi agar fragment menjadi defauld ketika di jalankan pertama
+        //berfungsi agar,fragment menjadi defauld ketika di jalankan pertama
         getSupportFragmentManager ().beginTransaction ().replace ( R.id.fragment_container, new ChatFragment () ).commit ();
-        //memberikan action jika nanti di klick apa yang akan terjadi
+        //memberikan action jika nanti di click apa yang akan terjadi
         bottomNavigation.setOnItemReselectedListener ( new NavigationBarView.OnItemReselectedListener () {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem item) {
-                //Membuat objek dari sebuah fragment
+                //membuat objek dari sebuah fragment
                Fragment selectedFragment = null;
 
                 switch ( (item.getItemId ())){
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                         selectedFragment = new callFragment ();
                         break;
                 }
-                //fungsi mengubah setipa fragment dengan dengan sebuah fungsi get,reolace itu bergantian sedangkan trancation menumpuk
+                //fungsi mengubah setiap fragment dengan sebuah fungsi get, replace itu bergantian sedangkan trancation menumpuk
 
 getSupportFragmentManager ().beginTransaction ().replace ( R.id.fragment_container, selectedFragment ).commit ();
 
